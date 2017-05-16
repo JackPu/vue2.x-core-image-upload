638cad512a301361af55d715c47d6cc5f9cdf43e
JackPu <kakashjack@gmail.com>
2017-05-16 14:47:20 +0800

    remove some files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-//var VueCoreImageUpload = require('./vue.core.image.upload');
-
-
-console.log('%c⚠️',"font-size:2.4em;");
-console.warn(`Please use "npm install vue-core-image-upload --save". vue2.x has been merged to https://github.com/Vanthink-UED/vue-core-image-upload.`);
-console.log('%c It just gets a better name and all api keep consistent.', 'font-style:italic');
-
-module.exports = {};
